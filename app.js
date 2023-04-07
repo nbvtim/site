3423aa2dbbf4532ad8074fac79d845f31c887959
Tim <exelent206@gmail.com>
2023-04-07 13:10:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const
+    c = console.log 
+/*
+
+xlsx > нужно создать логику откуда будут приходить данные 
+ошибка если файла нет на пк  =      xlsx = require('node-xlsx').parse(fs.readFileSync(filePath))[0]
+
+*/
+//     fs = require('fs')
+//     filePath = "C:/Users/User/Desktop/ДОКУМЕНТЫ/1 смена СВК/ОПИСИ/all.xlsx"
+//     xlsx = require('node-xlsx').parse(fs.readFileSync(filePath))[0]
+    
+// xlsx.date = Date.now()
+// xlsx.tg = "5965701331:AAG21HoAObaJtCGqB-KeVNx1hlabD8e8TB8"
+
+c("\033[1;32mВсе впорядке ... \033[m", Date.now())
