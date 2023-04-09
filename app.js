f5374d083a3bda2e3690adc42e144876409a1407
Tim <exelent206@gmail.com>
2023-04-09 15:16:07 +0300

    nbv

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const
     fs = require('fs')
     filePath = "C:/Users/User/Desktop/ДОКУМЕНТЫ/1 смена СВК/ОПИСИ/all.xlsx"
     xlsx = nodeXlsx.parse(fs.readFileSync(filePath))
-    xlsx.date = Date.now()
-    xlsx.tg = "5965701331:AAG21HoAObaJtCGqB-KeVNx1hlabD8e8TB8"
-//fs.writeFileSync("db.json",JSON.stringify(xlsx[0], null, " ") )
-c("\033[1;32mДанные обновлены\033[m", Date.now())
+xlsx[0].date = new Date()
+xlsx[0].tg = "5965701331:AAG21HoAObaJtCGqB-KeVNx1hlabD8e8TB8"
+fs.writeFileSync("db.json",JSON.stringify(xlsx[0], null, " ") )
+c("\033[1;32mДанные обновлены !!! \033[m", Date.now(), new Date())
